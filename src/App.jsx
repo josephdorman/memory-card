f54f4042dd83fe637590ab5f1c12eab0d7cf91f1
Joseph Dorman <JDorman555@yahoo.com>
2023-11-16 19:43:55 -0600

    create empty func/import into app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './styles/App.css'
 import Api from './components/api'
+import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
