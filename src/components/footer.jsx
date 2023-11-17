114497a2b401cc1e8359bdce86e2577cb507b26f
Joseph Dorman <JDorman555@yahoo.com>
2023-11-16 23:22:36 -0600

    create temp footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,12 @@
 import '../styles/footer.css'
+import github from '/icons8-github.svg'
 
 function Footer () {
   return (
     <>
+      <footer>
+        <p className='credit'>Create by<span className='bold'>Joseph Dorman</span><img src={github} className="s-logo"></img></p>
+      </footer>
     </>
   )
 }
