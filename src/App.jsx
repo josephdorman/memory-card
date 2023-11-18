1184545ebd84a724e2dd72cb6537c20e181388ce
Joseph Dorman <JDorman555@yahoo.com>
2023-11-18 00:14:30 -0600

    func to make shuffle smarter

--- src/App.jsx
+++ src/App.jsx
@@ -23,27 +23,64 @@ function App() {
   const setSeen = (id) => {
     const poke = pokemon.shuffledPokemon.find(obj => obj.id === id)
 
+    console.log(poke);
+
     poke.seen = true;
+  }
+
+  const checkSeen = (poke) => {
+    if (poke.seen === true) {
+      return true
+    }
+    else {
+      return false
+    }
+
+  }
+
+  const checkGameover = (id) => {
+    const poke = pokemon.shuffledPokemon.find(obj => obj.id === id)
+
+    if (checkSeen(poke) === true) {
+      console.log('seen')
+    }
+    else if (checkSeen(poke) === false) {
+      console.log('not seen')
+    }
+
   }
 
   // todo: make sure thats theres always 2 unseen pokemon
   const shuffle = () => {
     const shuffled = [];
+    let unseen = 2;
 
     while (shuffled.length < 3) {
       const newPoke = getRandomPokemon();
 
+      // if not in list continue
       if (shuffled.find(obj => obj.id === newPoke.id) === undefined) {
-        shuffled.push(newPoke)
+        // if not seen, push
+        if (!checkSeen(newPoke) && unseen !== 0) {
+          shuffled.push(newPoke)
+          unseen--;
+        } // if unseen = 0 then array already has 2 unseen cards, fill in the rest of the array w whatever
+        else if (unseen === 0) {
+          shuffled.push(newPoke)
+        }
+        
       }
 
     }
+
+    console.log(shuffled)
 
     setPokemon({...pokemon, shuffledPokemon: shuffled})
   }
 
   // return future "seen" var/this func will prob handle checking if card had been seen, once clicked and checked, shuffle/add score etc
   const clickHandler = (e, id) => {
+    checkGameover(id);
     setSeen(id);
     shuffle()
   }
